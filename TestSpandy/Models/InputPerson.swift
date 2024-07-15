5fbf8972702340b37e9d9d3238dbf3c18962db78
Kryusin <2210441@ecc.ac.jp>
2024-07-15 11:12:37 +0900

    add: SettingView!

---
+++ TestSpandy/Models/InputPerson.swift
@@ -0,0 +1,18 @@
+//
+//  InputPerson.swift
+//  TestSpandy
+//
+//  Created by IT on 2024/07/04.
+//
+
+import Foundation
+
+struct InputPerson {
+    var gender: [String]
+    var birthday: Date
+    var language: [String]
+    var qualification: [String]
+    var selfPR: String
+    var height: Int64
+    var weight: Int64
+}
